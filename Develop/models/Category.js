f3ac4d85ab7035fa1e121f3d95b5d86e8a2faf67
acamp <acampbell8610@gmail.com>
2022-03-16 21:35:50 -0500

    added routes

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -13,7 +13,7 @@ Category.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    categotry_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
